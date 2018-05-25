d3b843d8fba3207a3facb3f7615f027748e053df
Kwanhee <kwantangboby23@utexas.edu>
2018-05-24 20:53:47 -0500

    friends list

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//Friends list in the array
+var friendList = [
+    {
+        name: "Kwanhee",
+        profile: "https://scontent-sea1-1.cdninstagram.com/vp/756c494a0c099a6283db5ba568889f04/5B8A8F7A/t51.2885-15/e35/15535378_1159724024134949_6713745645628293120_n.jpg?se=8&ig_cache_key=MTQxNDgwOTY3Njc4NDE3MzUxMA%3D%3D.2",
+        socres: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "5",
+            "2",
+            "4",
+            "3",
+            "3",
+            "3"
+        ]
+    }
+]
+
+module.exports = friendList;
+
