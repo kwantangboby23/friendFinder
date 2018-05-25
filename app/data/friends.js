d09981fad11ddb4fb37256f67f334a893ed87c90
Kwanhee <kwantangboby23@utexas.edu>
2018-05-25 02:42:21 -0500

    picture showing

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friendsList = [
     {
         name: "Kwanhee",
-        profile: "https://scontent-sea1-1.cdninstagram.com/vp/756c494a0c099a6283db5ba568889f04/5B8A8F7A/t51.2885-15/e35/15535378_1159724024134949_6713745645628293120_n.jpg?se=8&ig_cache_key=MTQxNDgwOTY3Njc4NDE3MzUxMA%3D%3D.2",
+        photo: "https://scontent-sea1-1.cdninstagram.com/vp/756c494a0c099a6283db5ba568889f04/5B8A8F7A/t51.2885-15/e35/15535378_1159724024134949_6713745645628293120_n.jpg?se=8&ig_cache_key=MTQxNDgwOTY3Njc4NDE3MzUxMA%3D%3D.2",
         scores: [
             "4",
             "4",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,47 +4,47 @@ var bodyParser = require("body-parser");
 
 
 
-module.exports = function(app){
-    
-    app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
         res.json(friendsList);
     });
 
-    app.post("/api/friends", function(req, res){
-        
-    
-    var newScores = req.body.scores;
-    var scoresArray = [];
-    var friendCount = 0;
-    var bestMatch = 0;
+    app.post("/api/friends", function (req, res) {
 
-    
-    for(var i=0; i<friendsList.length; i++){
-      var difference = 0;
-      
-      for(var j=0; j<newScores.length; j++){
-        difference += (Math.abs(parseInt(friendsList[i].scores[j]) - parseInt(newScores[j])));
-      }
 
-      
-      scoresArray.push(difference);
-    }
+        var newScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
 
-  
-    for(var i=0; i<scoresArray.length; i++){
-      if(scoresArray[i] <= scoresArray[bestMatch]){
-        bestMatch = i;
-      }
-    }
 
- 
-    var bff = friendsList[bestMatch];
-    res.json(bff);
+        for (var i = 0; i < friendsList.length; i++) {
+            var difference = 0;
 
-   
-    friendsList.push(req.body);
-  });
-    
+            for (var j = 0; j < newScores.length; j++) {
+                difference += (Math.abs(parseInt(friendsList[i].scores[j]) - parseInt(newScores[j])));
+            }
+
+
+            scoresArray.push(difference);
+        }
+
+
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+
+        var bff = friendsList[bestMatch];
+        res.json(bff);
+
+
+        friendsList.push(req.body);
+    });
+
 
 
 };
